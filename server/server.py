b70f3f173ac80d91dfa4016074eea0f579fee854
Danilo Jakob <danilo.murer@gmail.com>
2021-06-04 02:54:50 +0200

    CHANGE: Keyword search in Logfile based on sample config file works

--- server/server.py
+++ server/server.py
@@ -2,6 +2,7 @@ import string
 
 import schedule
 import threading
+import configparser
 
 """
 Method is being called by scheduler with one config file
@@ -9,12 +10,27 @@ Method is being called by scheduler with one config file
 """
 def cron_job_task(config_file: string):
     print(config_file)
+    config = configparser.ConfigParser()
+    config.read(config_file)
 
+    log_file = config["Logging Configuration"]["logs.path"]
+    keywords = str(config["Logging Configuration"]["keyword"]).split(";")
+
+    file = open(log_file, "r")
+    lines = file.readlines()
+
+    for line in lines:
+        for keyword in keywords:
+            if keyword in line:
+                print("KEYWORD FOUND")
+                # send email
 
 def cron_job():
     print("I'm starting the cronjobtask")
+
+    # Get all available log files
     for i in range(10):
-        thread = threading.Thread(target=cron_job_task, args=("test",))
+        thread = threading.Thread(target=cron_job_task, args=("../sample/config.ini",))
         thread.start()
 
 
