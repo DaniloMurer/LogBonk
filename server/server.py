7f054a594059265446f0ea52d7dcfb7a6ab435f3
Danilo Jakob <danilo.murer@gmail.com>
2021-06-10 02:59:53 +0200

    ongoing development: Files cannot be moved because of PermisionError

--- server/server.py
+++ server/server.py
@@ -13,16 +13,17 @@ import email_config
 def send_mail(address, content):
     # send email
     print(address, content)
-    smtp_server = smtplib.SMTP(host=email_config.smtp_server, port=email_config.smtp_port)
-    smtp_server.login(email_config.smtp_username, email_config.smtp_password)
-    message = """\
-    Subject: LogBonk Alert
-    
-    {}
-    """.format(content)
-    smtp_server.sendmail(email_config.smtp_username, address, message)
-    smtp_server.close()
-
+    with smtplib.SMTP(host=email_config.smtp_server, port=email_config.smtp_port) as smtp_server:
+        smtp_server.connect(host=email_config.smtp_server, port=email_config.smtp_port)
+        smtp_server.starttls()
+        smtp_server.login(email_config.smtp_username, email_config.smtp_password)
+        message = """\
+        Subject: LogBonk Alert 
+        
+        {}
+        """.format(content)
+        smtp_server.sendmail(email_config.smtp_username, address, message)
+        smtp_server.close()
 
 
 """
@@ -46,27 +47,29 @@ def cron_job_task(config_file: string):
     for log_file in log_files:
         if '.log' in log_file:
             fqfn = log_files_path + '\\' + log_file
-            file = open(fqfn, "r")
-            lines = file.readlines()
+            if open(fqfn, "r").readable():
+                file = open(fqfn, "r")
+                lines = file.readlines()
 
-            # Check for keyword in log files
-            for line in lines:
-                for keyword in keywords:
-                    if keyword in line:
-                        # send email
-                        send_mail(email, line)
+                # Check for keyword in log files
+                for line in lines:
+                    for keyword in keywords:
+                        if keyword in line:
+                            # send email
+                            send_mail(email, line)
 
-            file.close()
+                file.close()
 
             # Move logs to archive folder
-            shutil.move(fqfn, archive_path + '\\' + log_file)
+            current_location = shutil.move(fqfn, archive_path + '\\' + log_file)
+            print(current_location)
 
     # Check Archive logs
     log_files_archive = os.listdir(archive_path)
     for log_file_archive in log_files_archive:
         fqafn = archive_path + '\\' + log_file_archive
 
-        if datetime.fromtimestamp(os.path.getctime(fqafn)) == (datetime.now() - timedelta(days=30)):
+        if datetime.fromtimestamp(os.path.getctime(fqafn)) <= (datetime.now() - timedelta(days=30)):
             os.remove(fqafn)
 
 
