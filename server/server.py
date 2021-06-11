35933378cc2516f01abe65e715f11994a3e858bd
Danilo Jakob <danilo.murer@gmail.com>
2021-06-11 14:04:33 +0200

    CHANGE: Fixed problem with sending the mail. CHANGE: Removed threads because they doin stuff they not supposed to

--- server/server.py
+++ server/server.py
@@ -7,6 +7,8 @@ import os
 import shutil
 from datetime import datetime, timedelta
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import email_config
 
 
@@ -19,18 +21,17 @@ def send_mail(address, content):
     :return: nothing
     """
     # send email
-    print(address, content)
+    print("Sending E-Mail")
     with smtplib.SMTP(host=email_config.smtp_server, port=email_config.smtp_port) as smtp_server:
         smtp_server.connect(host=email_config.smtp_server, port=email_config.smtp_port)
         smtp_server.starttls()
         smtp_server.login(email_config.smtp_username, email_config.smtp_password)
-        message = """\
-        Subject: LogBonk Alert 
-        
-        {}
-        """.format(content)
-        smtp_server.sendmail(email_config.smtp_username, address, message)
-        smtp_server.close()
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "LogBonk Alert"
+        message["To"] = address
+        message["From"] = email_config.smtp_username
+        message.attach(MIMEText(content, "plain"))
+        smtp_server.sendmail(email_config.smtp_username, address, message.as_string())
 
 
 def cron_job_task(config_file: string):
@@ -41,36 +42,33 @@ def cron_job_task(config_file: string):
     :return: nothing
     """
 
-    print(config_file)
-
     # Read Config file
     config = configparser.ConfigParser()
     config.read(config_file)
-    log_files_path = config["Logging Configuration"]["logs.path"]
+    log_files_path = str(config["Logging Configuration"]["logs.path"])
     log_files = os.listdir(log_files_path)
-    archive_path = config["Logging Configuration"]["logs.archive.path"]
+    archive_path = str(config["Logging Configuration"]["logs.archive.path"])
     keywords = str(config["Logging Configuration"]["keyword"]).split(";")
     email = str(config["Mail Configuration"]["mail.to"])
 
     for log_file in log_files:
         if '.log' in log_file:
             fqfn = log_files_path + '\\' + log_file
-            if open(fqfn, "r").readable():
-                file = open(fqfn, "r")
-                lines = file.readlines()
 
-                # Check for keyword in log files
-                for line in lines:
-                    for keyword in keywords:
-                        if keyword in line:
-                            # send email
-                            send_mail(email, line)
+            file = open(fqfn, "r")
+            lines = file.readlines()
 
-                file.close()
+            # Check for keyword in log files
+            for line in lines:
+                for keyword in keywords:
+                    if keyword in line:
+                        # send email
+                        send_mail(email, line)
 
+            file.close()
+            print("Moving files")
             # Move logs to archive folder
-            current_location = shutil.move(fqfn, archive_path + '\\' + log_file)
-            print(current_location)
+            shutil.move(fqfn, archive_path + '\\' + log_file)
 
     # Check Archive logs
     log_files_archive = os.listdir(archive_path)
@@ -96,12 +94,13 @@ def cron_job():
     config_files = os.listdir("../sample")
     for config_file in config_files:
         fqcn = "../sample/" + config_file
-        print(fqcn)
-        thread = threading.Thread(target=cron_job_task, args=(fqcn,))
-        thread.start()
+        #thread = threading.Thread(target=cron_job_task, args=(fqcn,))
+        #thread.start()
+        cron_job_task(fqcn)
 
+cron_job()
 
-schedule.every(5).seconds.do(cron_job)
+#schedule.every(5).seconds.do(cron_job)
 
-while True:
-    schedule.run_pending()
+#while True:
+#    schedule.run_pending()
